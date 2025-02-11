34601ea73031f8f0f64c1d42b35c79cd31b6c8f4
TailsonMoraes <tailsondeveloper1@gmail.com>
2025-02-11 11:57:22 -0300

    adicionando funcionalidades

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,6 +1,50 @@
-let original = document.querySelector(".loyalt-task");
+const btn = document.querySelector(".add");
 
 
-let loyalt = original.cloneNode(true);
+function addTask(){
 
-console.log(loyalt)
+
+    //CLONANDO LOYALT
+    let loyalt = document.querySelector(".loyalt-task");
+    let loyaltclone = loyalt.cloneNode(true);
+    //
+
+    //ADICIONANDO O TITULO DA TASK AO CLONE
+    let task = document.querySelector("#tarefa").value;
+    let taskscontainer = document.querySelector(".tasks");
+    loyaltclone.value = task;
+
+    //REMOVENDO CLASSES
+    loyaltclone.classList.remove("hide");
+    loyaltclone.classList.remove("done");
+    //
+
+    //FUNCIONALIDADE DO BOTÃO DE DELETAR
+    loyaltclone.querySelector(".delete-btn").addEventListener("click",function(e){
+        e.target.closest(".loyalt-task").remove();
+    });
+    //
+
+
+    //FUNCIONALIDADE DO BOTÃO DE CONCLUIR
+    loyaltclone.querySelector(".done-btn").addEventListener("click", function(e){
+        e.target.closest(".loyalt-task").classList.toggle("done");
+    })
+
+    //
+
+
+    //ADICIONANDO TASK
+    taskscontainer.appendChild(loyaltclone);
+
+
+    console.log(loyaltclone)
+
+}
+
+btn.addEventListener("click",(e) =>{
+    e.preventDefault();
+    addTask();
+})
+
+
